607cdbb8fffc6eb8d23b5fa4dad064e6e07abcb4
Harshda Mhase <harshadamhase12@gmail.com>
2023-04-16 22:54:00 -0900

    Objects in javascript using react

--- function.js
+++
@@ -1,9 +0,0 @@
-//Objects in js : in js objects are keyValue pair.key are alway in "" and valuess are
-//either boolean,number,string
-
-const student={"harsha":"121","yogi":"654","sj":"543","prajkta":"900"}
-console.log(student);
-console.log(student['harsha']);
-console.log(student['yogi']);
-console.log(student['sj']);
-console.log(student['prajkta']);
